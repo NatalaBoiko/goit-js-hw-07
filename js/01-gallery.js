a7e4b381245b3e26ffa21bd66e15df0e1066374a
NatalaBoiko <natalabojko@gmail.com>
2022-04-26 21:38:19 +0300

    01-started

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const makeImages = ({ preview, original, description }) => {
+  return `
+    <div class = 'gallery__item'>
+    <a class = 'gallery__link' href = ${original}>
+    <img src=${preview}
+    class="gallery__image" alt=${description} ></a>
+    </div>
+    `;
+};
+const makeImagesList = galleryItems.map(makeImages).join("");
+galleryEl.insertAdjacentHTML("beforeend", makeImagesList);
+
+const onOpenModal = (evt) => {
+  evt.preventDefault();
+  console.log(evt.target.nodeName);
+};
+galleryEl.addEventListener("click", onOpenModal);
