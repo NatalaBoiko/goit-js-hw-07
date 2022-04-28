ca8eaf83e48931273c0273c15a5361e4a195fadf
NatalaBoiko <natalabojko@gmail.com>
2022-04-28 15:56:26 +0300

    01-gallery  have done?

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
-// console.log(galleryItems);
+console.log(galleryItems);
 
 // Change code below this line
 
@@ -7,17 +7,44 @@ const galleryEl = document.querySelector(".gallery");
 const makeImages = ({ preview, original, description }) => {
   return `
     <div class = 'gallery__item'>
-    <a class = 'gallery__link' href = ${original}>
-    <img src=${preview}
-    class="gallery__image" alt=${description} ></a>
+    <a class = 'gallery__link' href = '${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'
+        />
+      </a>
     </div>
     `;
 };
+
 const makeImagesList = galleryItems.map(makeImages).join("");
 galleryEl.insertAdjacentHTML("beforeend", makeImagesList);
 
 const onOpenModal = (evt) => {
   evt.preventDefault();
-  console.log(evt.target.nodeName);
+  const modalImg = evt.target.dataset.source;
+  const modal = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src='${modalImg}'
+      data-source='${modalImg}'
+      alt='${evt.target.alt}'
+      width="800"
+      height="600">
+      `);
+
+  const onEscaPress = (evt) => {
+    console.log(evt);
+    if (evt.code === "Escape") {
+      window.removeEventListener("keydown", onEscaPress);
+      modal.close();
+    }
+  };
+  window.addEventListener("keydown", onEscaPress);
+
+  modal.show();
 };
+
 galleryEl.addEventListener("click", onOpenModal);
